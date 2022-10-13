149600e7e95d7542a4ab9ccdfd7b41dceeb52a21
bonjour-npy <2366353971@qq.com>
2022-10-13 15:34:26 +0800

    更新了第三个碎片以及主页

---
+++ README.md
@@ -0,0 +1,8 @@
+# Show Me You: Photo Sharing Platform
+Android Development大作业：图片分享平台
+
+指导老师：[张哲](https://github.com/Bamboo-fly)、贾飞
+
+主体使用了MVVM框架，使用了Retrofit2、RecyclerView等技术
+
+后端服务器使用老师提供[接口](http://47.107.52.7/api/modInfo?id=1)
---
+++ app/src/main/java/com/example/photoshare/activity/ShouCangActivity.java
@@ -0,0 +1,18 @@
+package com.example.photoshare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.photoshare.R;
+import com.example.photoshare.databinding.ActivityShouCangBinding;
+
+public class ShouCangActivity extends AppCompatActivity {
+
+    private ActivityShouCangBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shou_cang);
+    }
+}
